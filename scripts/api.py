1768587ea2331090fd0380060b8009d4b57cf419
WANG CHAO HSIEN <drgarbage@gmail.com>
2023-10-22 18:24:34 +0800

    skip when no api name

--- scripts/api.py
+++ scripts/api.py
@@ -1,46 +1,30 @@
-from fastapi import FastAPI
-from rembg import remove
-from PIL import PngImagePlugin, Image
-
-import gradio as gr
 import io
 import base64
-
 import asyncio
-import aio_pika
 import threading
 import json
-import aioipfs
 import requests
+import gradio as gr
+import aio_pika
+import aioipfs
+
 from io import BytesIO
+from PIL import PngImagePlugin, Image
+from fastapi import FastAPI
+from rembg import remove
 from multiaddr import Multiaddr
 
 
 def decode_base64_to_image(encoding):
-    if encoding.startswith("http://") or encoding.startswith("https://"):
-        if not opts.api_enable_requests:
-            raise HTTPException(status_code=500, detail="Requests not allowed")
-
-        if opts.api_forbid_local_requests and not verify_url(encoding):
-            raise HTTPException(status_code=500, detail="Request to local resource not allowed")
-
-        headers = {'user-agent': opts.api_useragent} if opts.api_useragent else {}
-        response = requests.get(encoding, timeout=30, headers=headers)
-        try:
-            image = Image.open(BytesIO(response.content))
-            return image
-        except Exception as e:
-            raise HTTPException(status_code=500, detail="Invalid image url") from e
-
     if encoding.startswith("data:image/"):
         encoding = encoding.split(";")[1].split(",")[1]
     try:
         image = Image.open(BytesIO(base64.b64decode(encoding)))
         return image
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Invalid encoded image") from e
+        return None
     
-
+    
 def image_buffer(image):
     from modules.shared import opts
     use_metadata = False
@@ -61,31 +45,41 @@ def image_buffer(image):
     return buf
 
 
+def get_progress():
+    if shared.state.job_count == 0:
+        return 0, None
+
+    # avoid dividing zero
+    progress = 0.01
+    progressImage = None
+
+    if shared.state.job_count > 0:
+        progress += shared.state.job_no / shared.state.job_count
+    if shared.state.sampling_steps > 0:
+        progress += 1 / shared.state.job_count * shared.state.sampling_step / shared.state.sampling_steps
+
+    progress = min(progress, 1)
+
+    shared.state.set_current_image()
+    if shared.state.current_image:
+        progressImage = shared.state.current_image
+
+    return progress, progressImage
+
+
 async def upload_images(images: list):
     hashes = []
-    client = aioipfs.AsyncIPFS(maddr=Multiaddr('/dns4/ai.printii.com/tcp/5001/http'))
-
-    for image in images:
-        file = image_buffer(image)
-        entry = await client.core.add_bytes(file.getvalue(), to_files='/photo.png')
-        filename = entry['Name']
-        filehash = entry['Hash']
-        hashes.append(filehash)
-        print(f'File uploaded {entry}')
+    async with aioipfs.AsyncIPFS(maddr=Multiaddr('/dns4/ai.printii.com/tcp/5001/http')) as client:
+        for image in images:
+            file = image_buffer(image)
+            entry = await client.core.add_bytes(file.getvalue(), to_files='/photo.png')
+            filename = entry['Name']
+            filehash = entry['Hash']
+            hashes.append(filehash)
+            print(f'File uploaded {entry}')
     
-    await client.close()
     print(f'Processed images: {hashes}')
     return hashes
-
-
-def decode_base64_to_image(encoding):
-    if encoding.startswith("data:image/"):
-        encoding = encoding.split(";")[1].split(",")[1]
-    try:
-        image = Image.open(BytesIO(base64.b64decode(encoding)))
-        return image
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Invalid encoded image") from e
 
 
 
@@ -94,10 +88,10 @@ def process_txt2img(args):
     from modules.shared import opts
     from modules.processing import StableDiffusionProcessingTxt2Img, process_images
     from contextlib import closing
-    
     script_name = args.get('script_name', None)
     script_args = args.get('script_args', [])
     alwayson_scripts = args.get('alwayson_scripts', None)
+    script = None
 
     script_runner = scripts.scripts_txt2img
     if not script_runner.scripts:
@@ -152,7 +146,7 @@ def process_txt2img(args):
             shared.state.begin(job="scripts_txt2img")
             if script_name is not None:
                 p.script_args = full_script_args
-                processed = scripts.scripts_txt2img.run(p, *p.script)
+                processed = scripts.scripts_txt2img.run(p, *p.script_args)
             else:
                 p.script_args = tuple(full_script_args)
                 processed = process_images(p)
@@ -163,12 +157,59 @@ def process_txt2img(args):
     return processed
 
 
-
 def process_img2img(args):
     from modules import scripts, shared
     from modules.shared import opts
     from modules.processing import StableDiffusionProcessingImg2Img, process_images
     from contextlib import closing
+    script_name = args.get('script_name', None)
+    script_args = args.get('script_args', [])
+    alwayson_scripts = args.get('alwayson_scripts', None)
+    script = None
+
+    script_runner = scripts.scripts_txt2img
+    if not script_runner.scripts:
+        script_runner.initialize_scripts(False)
+
+    # init script args
+    max_args = 1
+    for script in script_runner.scripts:
+        if script.args_to is not None and max_args < script.args_to:
+            max_args = script.args_to
+            
+    full_script_args = [None]*max_args
+    full_script_args[0] = 0
+
+    # get default values
+    with gr.Blocks(): # will throw errors calling ui function without this
+        for script in script_runner.scripts:
+            if script.ui(script.is_img2img):
+                ui_default_values = []
+                for elem in script.ui(script.is_img2img):
+                    ui_default_values.append(elem.value)
+                full_script_args[script.args_from:script.args_to] = ui_default_values
+                
+
+    if script_name:
+        script_index = [script.title().lower() for script in script_runner.selectable_scripts].index(script_name.lower())
+        script = script_runner.selectable_scripts[script_index]
+        full_script_args[script.args_from:script.args_to] = script_args
+        full_script_args[0] = script_index + 1
+
+    if alwayson_scripts:
+        for alwayson_script_name in alwayson_scripts.keys():
+            alwayson_script = script_runner.scripts[[script.title().lower() for script in script_runner.scripts].index(alwayson_script_name.lower())]
+            if "args" in alwayson_scripts[alwayson_script_name]:
+                for idx in range(0, min((alwayson_script.args_to - alwayson_script.args_from), len(alwayson_scripts[alwayson_script_name]['args']))):
+                    full_script_args[alwayson_script.args_from + idx] = alwayson_scripts[alwayson_script_name]['args'][idx]
+
+    args.pop('script_name', None)
+    args.pop('script_args', None)
+    args.pop('alwayson_scripts', None)
+    args.pop('send_images', True)
+    args.pop('save_images', None)
+
+    processed = []
 
     with closing(StableDiffusionProcessingImg2Img(sd_model=shared.sd_model, **args)) as p:
         p.init_images = [decode_base64_to_image(x) for x in args.get('init_images')]
@@ -178,13 +219,37 @@ def process_img2img(args):
 
         try:
             shared.state.begin(job="scripts_img2img")
-            processed = process_images(p)
+            if script_name is not None:
+                p.script_args = full_script_args
+                processed = scripts.scripts_img2img.run(p, *p.script_args)
+            else:
+                p.script_args = tuple(full_script_args)
+                processed = process_images(p)
         finally:
             shared.state.end()
             shared.total_tqdm.clear()
 
     return processed
 
+
+async def report_progress(report_url):
+    while True:
+        progress, progressImage = get_progress()
+
+        progressImageHash = None
+        if progressImage is not None:
+            hashes = await upload_images([progressImage])
+            progressImageHash = hashes[0]
+
+        data = { "progress": progress, "progressImage": progressImageHash }
+
+        try:
+            response = requests.post(report_url, json=data)
+            print(f"[x] Progress: {progress}% - {response}")
+        except Exception as e:
+            print("[x] Error occur on reporting progress.")
+            print(e)
+        await asyncio.sleep(1)
 
 
 async def consume():
@@ -194,37 +259,49 @@ async def consume():
         channel = await connection.channel()
         queue = await channel.declare_queue('generation', durable=False)
 
-        print(' [x] SpellForge Generation Worker is running.')
+        print('[x] SpellForge Generation Worker is running.')
 
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    print(f"[x] Accepted Generation Request...")
+                    try:
+                        print(f"[x] Accepted Generation Request...")
 
-                    msg = json.loads(message.body.decode())
-                    api = msg.get('api')
-                    args = msg.get('params') 
-                    # reportProgress = msg.get('reportProgress')
-                    reportResult = msg.get('reportResult')
+                        msg = json.loads(message.body.decode())
+                        api = msg.get('api')
+                        args = msg.get('params') 
+                        reportProgress = msg.get('reportProgress')
+                        reportResult = msg.get('reportResult')
 
-                    print(f"[x] Processing SpellForge {api}...")
+                        print(f"[x] Processing SpellForge {api}...")
 
-                    processed = []
+                        if not api:
+                            print("[x] API is None. Skipping this message.")
+                            continue  # Skip the current iteration and move to the next message
 
-                    if(api == 'txt2img'):
-                        processed = process_txt2img(args)
+                        progress_task = asyncio.create_task(report_progress(reportProgress))
+                        processed = []
 
-                    if(api == 'img2img'):
-                        processed = process_img2img(args)
+                        if api == 'txt2img':
+                            processed = process_txt2img(args)
 
-                    hashes = await upload_images(processed.images)
-                    data = { "result": { "images": hashes } }
-                    response = requests.post(reportResult, json=data)
-                    print(f"[x] Finall Result: {response}")
+                        if api == 'img2img':
+                            processed = process_img2img(args)
 
+                        progress_task.cancel()
+
+                        if processed.images:
+                            hashes = await upload_images(processed.images)
+                        else:
+                            hashes = []
+                        print(f"hashes {hashes}")
+                        data = { "result": { "images": hashes } }
+                        response = requests.post(reportResult, json=data)
+                        print(f"[x] Finall Result: {response}")
+                    except Exception as e:
+                        print("[x] Error occur on processing task.")
+                        print(e)
                     
-
-
 
 def run_consume_loop():
     loop = asyncio.new_event_loop()
@@ -239,7 +316,6 @@ def regist_worker():
 
     # Optionally, return the consume_thread in case you want to join it later
     return consume_thread
-
 
 
 def rembg_api(_: gr.Blocks, app: FastAPI):
@@ -277,14 +353,9 @@ def rembg_api(_: gr.Blocks, app: FastAPI):
     regist_worker()
 
 
-
 try:
     import modules.script_callbacks as script_callbacks
 
     script_callbacks.on_app_started(rembg_api)
 except:
     pass
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
